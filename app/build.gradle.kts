00127b6518ac476b62d0cd5187287b726d28d824
siddhant <Sidhantjha95@gmail.com>
2024-10-08 14:06:56 +0530

    With Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -50,29 +51,31 @@ android {
 }
 
 dependencies {
-    // compose View model
-    implementation("androidx.lifecycle:lifecycle-viewmodel-android:2.6.2")
-    // Network calls
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+
+    //Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    //Network calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
-    // JSON to Kotlin object mapping
+    //Json to Kotlin object mapping
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    // Image loading
+
+    //Image loading
     implementation("io.coil-kt:coil-compose:2.4.0")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
-    implementation ("androidx.fragment:fragment-ktx:1.4.1")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
--- app/src/main/java/eu/tutorials/myrecipeapp/MainViewModel.kt
+++ app/src/main/java/eu/tutorials/myrecipeapp/MainViewModel.kt
@@ -5,12 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
-// state compose runtime import for state
 
-    private val _categoriesState = mutableStateOf(RecipeState())
-    val categoriesState : State<RecipeState> = _categoriesState
+    private val _categorieState = mutableStateOf(RecipeState())
+    val categoriesState: State<RecipeState> = _categorieState
 
     init {
         fetchCategories()
@@ -21,26 +21,25 @@ class MainViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val response = recipeService.getCategories()
-                _categoriesState.value = _categoriesState.value.copy(
+                _categorieState.value = _categorieState.value.copy(
                     list = response.categories,
                     loading = false,
                     error = null
                 )
 
-            }catch (e : Exception){
-                _categoriesState.value = _categoriesState.value.copy(
+            }catch (e: Exception){
+                _categorieState.value = _categorieState.value.copy(
                     loading = false,
                     error = "Error fetching Categories ${e.message}"
                 )
-
             }
         }
     }
 
-
     data class RecipeState(
-        val loading : Boolean = true,
-        val list : List<Category> = emptyList(),
-        val error : String? = null
+        val loading: Boolean = true,
+        val list: List<Category> = emptyList(),
+        val error: String? = null
     )
+
 }
